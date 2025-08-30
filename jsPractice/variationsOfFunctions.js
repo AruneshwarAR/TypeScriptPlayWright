463af8d27639b156b5b413af11540e83f03809a7
ARUNESHWAR AR <aruneshwarar@ARs-MBP.local>
2025-08-30 15:01:45 +0530

    normal function and arrow function and function assigned to let variable

---
+++ jsPractice/variationsOfFunctions.js
@@ -0,0 +1,9 @@
+function add(a, b) {
+  return "the addition is " + (a + b);
+}
+
+console.log(add(5, 6));
+console.log("-------");
+
+let sum = (c, d) => "the sum is " + (c + d);
+console.log(sum(10, 20));
