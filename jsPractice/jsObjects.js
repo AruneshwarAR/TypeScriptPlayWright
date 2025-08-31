26627a6dc2e97a1c6a1b2aa8d31ee055d68925cb
Aruneshwar AR <hiaruneshwar@gmail.com>
2025-08-31 20:16:43 +0530

    completed objects

--- jsPractice/jsObjects.js
+++ jsPractice/jsObjects.js
@@ -1,6 +1,10 @@
 let person = {
   firstName: "Aruneshwar",
   lastName: "AR",
+  qualification: "BE.EEE",
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
+  },
 };
 
 console.log(person.firstName);
@@ -12,5 +16,17 @@ person.age = "25";
 console.log(person);
 person.age = 25;
 console.log(person);
+console.log("age" in person);
 delete person.age;
+console.log("age" in person);
 console.log(person);
+
+for (let key in person) {
+  try {
+    console.log(person[key]());
+  } catch {
+    console.log("*********");
+    console.log(person[key]);
+    console.log("*********");
+  }
+}
