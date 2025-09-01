91c3426e9f7620b1c061816bc7993b02b6fc603e
Aruneshwar AR <hiaruneshwar@gmail.com>
2025-09-01 21:50:03 +0530

    print login error block

--- jsCode/tests/locators.spec.js
+++ jsCode/tests/locators.spec.js
@@ -1,9 +1,10 @@
 import { test, expect } from "@playwright/test";
 test.only("browser launch", async ({ page }) => {
   await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
-  console.log(page.title());
+  console.log(await page.title());
   await page.locator("#username").fill("asfasfas");
   await page.locator("#password").fill("fasfaf");
   await page.locator("[type='checkbox']").check();
   await page.locator("[value='Sign In']").click();
+  console.log(await page.locator("[style*='block']"));
 });
