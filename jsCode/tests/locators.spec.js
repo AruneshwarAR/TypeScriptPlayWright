a796b531460d9e86e46464ed02e0e9a87c321ea0
Aruneshwar AR <hiaruneshwar@gmail.com>
2025-09-01 22:36:36 +0530

    stored locators into const variable

--- jsCode/tests/locators.spec.js
+++ jsCode/tests/locators.spec.js
@@ -1,11 +1,22 @@
 import { test, expect } from "@playwright/test";
 test.only("browser launch", async ({ page }) => {
+  const userName = page.locator("#username");
+  const passWord = page.locator("#password");
+  const signIn = page.locator("[value='Sign In']");
+
   await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
   console.log(await page.title());
-  await page.locator("#username").fill("asfasfas");
-  await page.locator("#password").fill("fasfaf");
+  await userName.fill("asfasfas");
+  await passWord.fill("fasfaf");
   await page.locator("[type='checkbox']").check();
-  await page.locator("[value='Sign In']").click();
+  await signIn.click();
   console.log(await page.locator("[style*='block']").textContent()); // to get text from the element
   await expect(page.locator("[style*='block']")).toContainText("Incorrec"); // assert webelement contains text
+  await userName.fill("rahulshettyacademy");
+  await passWord.fill("learning");
+  await signIn.click();
+  let links = await page.locator(".card-title a").nth(1).textContent();
+  let linkss = await page.locator(".card-title a").first().textContent();
+  console.log(linkss);
+  console.log(links);
 });
