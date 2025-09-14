76f1d9e3a2d40088696a93e984a5b6f1dd3e453b
Aruneshwar AR <hiaruneshwar@gmail.com>
2025-09-14 22:01:23 +0530

    added specialLocator test

---
+++ jsCode/tests/specialLocator.spec.js
@@ -0,0 +1,5 @@
+import { test, expect } from "@playwright/test";
+test.only("playwright special locators", async ({ page }) => {
+  await page.goto("https://rahulshettyacademy.com/angularpractice/");
+  await page.getByLabel("Check me").check();
+});
