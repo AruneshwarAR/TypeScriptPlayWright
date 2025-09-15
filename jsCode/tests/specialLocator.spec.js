75cfd5fa2531cdaeb4be22b86dced7a41880bf13
Aruneshwar AR <hiaruneshwar@gmail.com>
2025-09-15 22:25:27 +0530

    click gender select option

--- jsCode/tests/specialLocator.spec.js
+++ jsCode/tests/specialLocator.spec.js
@@ -3,4 +3,5 @@ test.only("playwright special locators", async ({ page }) => {
   await page.goto("https://rahulshettyacademy.com/angularpractice/");
   await page.getByLabel("Check me").check();
   await page.getByLabel("Employed").click();
+  await page.getByLabel("Gender").selectOption("Male");
 });
