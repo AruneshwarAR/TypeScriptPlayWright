b88d796e996d18abf06c14cf65af5a008b0f5161
Aruneshwar AR <hiaruneshwar@gmail.com>
2025-09-15 22:24:24 +0530

    click Employed

--- jsCode/tests/specialLocator.spec.js
+++ jsCode/tests/specialLocator.spec.js
@@ -2,4 +2,5 @@ import { test, expect } from "@playwright/test";
 test.only("playwright special locators", async ({ page }) => {
   await page.goto("https://rahulshettyacademy.com/angularpractice/");
   await page.getByLabel("Check me").check();
+  await page.getByLabel("Employed").click();
 });
